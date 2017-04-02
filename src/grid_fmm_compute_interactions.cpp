e9cc6e242d8b5bece807b3096a574990d440d504
fire <fire@element.com>
2017-04-02 10:15:21 +0200

    before changing iteration type, working commit

--- src/grid_fmm_compute_interactions.cpp
+++ src/grid_fmm_compute_interactions.cpp
@@ -10,6 +10,7 @@
 
 #include <cstddef>
 #include <utility>
+#include <chrono>
 
 // non-root and non-leaf node (David)
 extern std::vector<interaction_type> ilist_n;
@@ -35,6 +36,8 @@ void grid::compute_interactions(gsolve_type type) {
 }    
 
 void grid::compute_interactions_inner(gsolve_type type) {
+
+    auto start = std::chrono::high_resolution_clock::now();
 
     // calculating the contribution of all the inner cells
     // calculating the interaction
@@ -80,7 +83,7 @@ void grid::compute_interactions_inner(gsolve_type type) {
     interaction_list[cur_index_start].inner_loop_stop = list_size;
 
     // std::cout << "list_size:" << list_size << std::endl;
-    for (size_t li = 0; li < list_size; li += simd_len) {
+    for (size_t interaction_first_index = 0; interaction_first_index < list_size; interaction_first_index += simd_len) {
 	// dX is distance between X and Y
 	// X and Y are the two cells interacting
 	// X and Y store the 3D center of masses (per simd element, SoA style)
@@ -88,14 +91,8 @@ void grid::compute_interactions_inner(gsolve_type type) {
 	std::array<simd_vector, NDIM> X;
 	// m multipole moments of the cells
 	taylor<4, simd_vector> m1;
-	// n angular momentum of the cells
-	taylor<4, simd_vector> n0;
 
-	auto& M = *M_ptr;
-	
-	std::array<simd_vector, NDIM> Y;
-	taylor<4, simd_vector> m0;
-	taylor<4, simd_vector> n1;
+	auto& M = *M_ptr;       
 
 	// vector of multipoles (== taylor<4>s)
 	// here the multipoles are used as a storage of expansion coefficients
@@ -103,8 +100,8 @@ void grid::compute_interactions_inner(gsolve_type type) {
 
 	// TODO: only uses first 10 coefficients? ask Dominic
 	// load variables for the first multipole
-	for (integer i = 0; i != simd_len && integer(li + i) < list_size; ++i) {
-	    const integer iii0 = interaction_list[li + i].first;
+	for (integer i = 0; i != simd_len && integer(interaction_first_index + i) < list_size; ++i) {
+	    const integer iii0 = interaction_list[interaction_first_index + i].first;
 	    // std::cout << "first: " << iii0 << " second: " << interaction_list[li + i].second << std::endl;
 	    space_vector const& com0iii0 = com0[iii0];
 	    for (integer d = 0; d < NDIM; ++d) {
@@ -117,172 +114,203 @@ void grid::compute_interactions_inner(gsolve_type type) {
 	    for (integer j = 0; j != taylor_sizes[3]; ++j) {
     		m1[j][i] = Miii0[j];
 	    }
-
-	    // RHO computes the gravitational potential based on the mass density
-	    // non-RHO computes the time derivative (hydrodynamics code V_t)
-	    if (type == RHO) {
-		// this branch computes the angular momentum correction, (20) in the paper
-		// divide by mass of other cell
-		real const tmp1 = Miii1() / Miii0();
-		// calculating the coefficients for formula (M are the octopole moments)
-		// the coefficients are calculated in (17) and (18)
-		for (integer j = taylor_sizes[2]; j != taylor_sizes[3]; ++j) {
-		    n0[j][i] = Miii1[j] - Miii0[j] * tmp1;
-		}
-	    }
 	}
 
-	// load variables for the second multipole
-	for (integer i = 0; i != simd_len && integer(li + i) < list_size; ++i) {
-	    const integer iii1 = interaction_list[li + i].second;
-	    space_vector const& com0iii1 = com0[iii1];
+	// stop index of the iteration -> points to first entry in interaction_list where the first multipole doesn't match the current multipole
+	size_t inner_loop_stop = interaction_list[interaction_first_index].inner_loop_stop;
+
+	// to a simd-sized step in the sublist of the interaction list where the outer multipole is same
+	// TODO: fix loop bound
+	for (size_t interaction_second_index = 0; interaction_second_index < 1; interaction_second_index += 4) {
+	
+	    std::array<simd_vector, NDIM> Y;
+	    taylor<4, simd_vector> m0;
+	
+	    // load variables for the second multipole
+	    for (integer i = 0; i != simd_len && integer(interaction_first_index + i) < list_size; ++i) {
+		const integer iii1 = interaction_list[interaction_first_index + i].second;
+		space_vector const& com0iii1 = com0[iii1];
+		for (integer d = 0; d < NDIM; ++d) {
+		    // load the 3D center of masses
+		    Y[d][i] = com0iii1[d];
+		}
+
+		// cell specific taylor series coefficients
+		multipole const& Miii1 = M[iii1];
+		for (integer j = 0; j != taylor_sizes[3]; ++j) {
+		    m0[j][i] = Miii1[j];
+		}
+	    }
+
+	    taylor<4, simd_vector> n1;
+	    // n angular momentum of the cells
+	    taylor<4, simd_vector> n0;
+	
+	    // Initalize moments
+	    for (integer i = 0; i != simd_len && integer(interaction_first_index + i) < list_size; ++i) {
+		if (type != RHO) {
+		    //TODO: Can we avoid that?
+#pragma GCC ivdep
+		    for (integer j = taylor_sizes[2]; j != taylor_sizes[3]; ++j) {
+			n0[j] = ZERO;
+			n1[j] = ZERO;
+		    }
+		} else {
+		    //TODO: let's hope this is enter rarely
+		    const integer iii0 = interaction_list[interaction_first_index + i].first;
+		    const integer iii1 = interaction_list[interaction_first_index + i].second;
+		    multipole const& Miii0 = M[iii0];
+		    multipole const& Miii1 = M[iii1];
+		    // this branch computes the angular momentum correction, (20) in the paper
+		    // divide by mass of other cell
+		    real const tmp1 = Miii1() / Miii0();
+		    real const tmp2 = Miii0() / Miii1();
+		    // calculating the coefficients for formula (M are the octopole moments)
+		    // the coefficients are calculated in (17) and (18)
+		    for (integer j = taylor_sizes[2]; j != taylor_sizes[3]; ++j) {
+			n0[j][i] = Miii1[j] - Miii0[j] * tmp1;
+			n1[j][i] = Miii0[j] - Miii1[j] * tmp2;
+		    }
+		}
+	    }
+
+	    // distance between cells in all dimensions
+	    std::array<simd_vector, NDIM> dX;
+#pragma GCC ivdep
 	    for (integer d = 0; d < NDIM; ++d) {
-		// load the 3D center of masses
-		Y[d][i] = com0iii1[d];
+		dX[d] = X[d] - Y[d];
 	    }
 
-	    // cell specific taylor series coefficients
-	    multipole const& Miii1 = M[iii1];
-	    for (integer j = 0; j != taylor_sizes[3]; ++j) {
-    		m0[j][i] = Miii1[j];
+	    // R_i in paper is the dX in the code
+	    // D is taylor expansion value for a given X expansion of the gravitational
+	    // potential (multipole expansion)
+	    taylor<5, simd_vector> D;
+	    // A0, A1 are the contributions to L (for each cell)
+	    taylor<4, simd_vector> A0, A1;
+	    // B0, B1 are the contributions to L_c (for each cell)
+	    std::array<simd_vector, NDIM> B0 = {
+		simd_vector(ZERO), simd_vector(ZERO), simd_vector(ZERO)};
+	    std::array<simd_vector, NDIM> B1 = {
+		simd_vector(ZERO), simd_vector(ZERO), simd_vector(ZERO)};
+
+	    // calculates all D-values, calculate all coefficients of 1/r (not the potential),
+	    // formula (6)-(9) and (19)
+	    D.set_basis(dX);
+
+	    // the following loops calculate formula (10), potential from A->B and B->A
+	    // (basically alternating)
+	    A0[0] = m0[0] * D[0];
+	    A1[0] = m1[0] * D[0];
+	    if (type != RHO) {
+		for (integer i = taylor_sizes[0]; i != taylor_sizes[1]; ++i) {
+		    A0[0] -= m0[i] * D[i];
+		    A1[0] += m1[i] * D[i];
+		}
 	    }
+	    for (integer i = taylor_sizes[1]; i != taylor_sizes[2]; ++i) {
+		const auto tmp = D[i] * (factor[i] * HALF);
+		A0[0] += m0[i] * tmp;
+		A1[0] += m1[i] * tmp;
+	    }
+	    for (integer i = taylor_sizes[2]; i != taylor_sizes[3]; ++i) {
+		const auto tmp = D[i] * (factor[i] * SIXTH);
+		A0[0] -= m0[i] * tmp;
+		A1[0] += m1[i] * tmp;
+	    }
+
+	    for (integer a = 0; a < NDIM; ++a) {
+		int const* ab_idx_map = to_ab_idx_map3[a];
+		int const* abc_idx_map = to_abc_idx_map3[a];
+
+		A0(a) = m0() * D(a);
+		A1(a) = -m1() * D(a);
+		for (integer i = 0; i != 6; ++i) {
+		    if (type != RHO && i < 3) {
+			A0(a) -= m0(a) * D[ab_idx_map[i]];
+			A1(a) -= m1(a) * D[ab_idx_map[i]];
+		    }
+		    const integer cb_idx = cb_idx_map[i];
+		    const auto tmp1 = D[abc_idx_map[i]] * (factor[cb_idx] * HALF);
+		    A0(a) += m0[cb_idx] * tmp1;
+		    A1(a) -= m1[cb_idx] * tmp1;
+		}
+	    }
+
 	    if (type == RHO) {
-		real const tmp2 = Miii0() / Miii1();
-		for (integer j = taylor_sizes[2]; j != taylor_sizes[3]; ++j) {
-
-		    n1[j][i] = Miii0[j] - Miii1[j] * tmp2;
+		for (integer a = 0; a != NDIM; ++a) {
+		    int const* abcd_idx_map = to_abcd_idx_map3[a];
+		    for (integer i = 0; i != 10; ++i) {
+			const integer bcd_idx = bcd_idx_map[i];
+			const auto tmp = D[abcd_idx_map[i]] * (factor[bcd_idx] * SIXTH);
+			B0[a] -= n0[bcd_idx] * tmp;
+			B1[a] -= n1[bcd_idx] * tmp;
+		    }
 		}
 	    }
-	}
 
-	if (type != RHO) {
-#pragma GCC ivdep
-	    for (integer j = taylor_sizes[2]; j != taylor_sizes[3]; ++j) {
-		n0[j] = ZERO;
-		n1[j] = ZERO;
-	    }
-	}
-
-	// distance between cells in all dimensions
-	std::array<simd_vector, NDIM> dX;
-#pragma GCC ivdep
-	for (integer d = 0; d < NDIM; ++d) {
-	    dX[d] = X[d] - Y[d];
-	}
-
-	// R_i in paper is the dX in the code
-	// D is taylor expansion value for a given X expansion of the gravitational
-	// potential (multipole expansion)
-	taylor<5, simd_vector> D;
-	// A0, A1 are the contributions to L (for each cell)
-	taylor<4, simd_vector> A0, A1;
-	// B0, B1 are the contributions to L_c (for each cell)
-	std::array<simd_vector, NDIM> B0 = {
-	    simd_vector(ZERO), simd_vector(ZERO), simd_vector(ZERO)};
-	std::array<simd_vector, NDIM> B1 = {
-	    simd_vector(ZERO), simd_vector(ZERO), simd_vector(ZERO)};
-
-	// calculates all D-values, calculate all coefficients of 1/r (not the potential),
-	// formula (6)-(9) and (19)
-	D.set_basis(dX);
-
-	// the following loops calculate formula (10), potential from A->B and B->A
-	// (basically alternating)
-	A0[0] = m0[0] * D[0];
-	A1[0] = m1[0] * D[0];
-	if (type != RHO) {
-	    for (integer i = taylor_sizes[0]; i != taylor_sizes[1]; ++i) {
-		A0[0] -= m0[i] * D[i];
-		A1[0] += m1[i] * D[i];
-	    }
-	}
-	for (integer i = taylor_sizes[1]; i != taylor_sizes[2]; ++i) {
-	    const auto tmp = D[i] * (factor[i] * HALF);
-	    A0[0] += m0[i] * tmp;
-	    A1[0] += m1[i] * tmp;
-	}
-	for (integer i = taylor_sizes[2]; i != taylor_sizes[3]; ++i) {
-	    const auto tmp = D[i] * (factor[i] * SIXTH);
-	    A0[0] -= m0[i] * tmp;
-	    A1[0] += m1[i] * tmp;
-	}
-
-	for (integer a = 0; a < NDIM; ++a) {
-	    int const* ab_idx_map = to_ab_idx_map3[a];
-	    int const* abc_idx_map = to_abc_idx_map3[a];
-
-	    A0(a) = m0() * D(a);
-	    A1(a) = -m1() * D(a);
 	    for (integer i = 0; i != 6; ++i) {
-		if (type != RHO && i < 3) {
-		    A0(a) -= m0(a) * D[ab_idx_map[i]];
-		    A1(a) -= m1(a) * D[ab_idx_map[i]];
-		}
-		const integer cb_idx = cb_idx_map[i];
-		const auto tmp1 = D[abc_idx_map[i]] * (factor[cb_idx] * HALF);
-		A0(a) += m0[cb_idx] * tmp1;
-		A1(a) -= m1[cb_idx] * tmp1;
-	    }
-	}
+		int const* abc_idx_map6 = to_abc_idx_map6[i];
 
-	if (type == RHO) {
-	    for (integer a = 0; a != NDIM; ++a) {
-		int const* abcd_idx_map = to_abcd_idx_map3[a];
-		for (integer i = 0; i != 10; ++i) {
-		    const integer bcd_idx = bcd_idx_map[i];
-		    const auto tmp = D[abcd_idx_map[i]] * (factor[bcd_idx] * SIXTH);
-		    B0[a] -= n0[bcd_idx] * tmp;
-		    B1[a] -= n1[bcd_idx] * tmp;
+		integer const ab_idx = ab_idx_map6[i];
+		A0[ab_idx] = m0() * D[ab_idx];
+		A1[ab_idx] = m1() * D[ab_idx];
+		for (integer c = 0; c < NDIM; ++c) {
+		    const auto& tmp = D[abc_idx_map6[c]];
+		    A0[ab_idx] -= m0(c) * tmp;
+		    A1[ab_idx] += m1(c) * tmp;
 		}
 	    }
-	}
 
-	for (integer i = 0; i != 6; ++i) {
-	    int const* abc_idx_map6 = to_abc_idx_map6[i];
-
-	    integer const ab_idx = ab_idx_map6[i];
-	    A0[ab_idx] = m0() * D[ab_idx];
-	    A1[ab_idx] = m1() * D[ab_idx];
-	    for (integer c = 0; c < NDIM; ++c) {
-		const auto& tmp = D[abc_idx_map6[c]];
-		A0[ab_idx] -= m0(c) * tmp;
-		A1[ab_idx] += m1(c) * tmp;
+	    for (integer i = taylor_sizes[2]; i != taylor_sizes[3]; ++i) {
+		const auto& tmp = D[i];
+		A1[i] = -m1[0] * tmp;
+		A0[i] = m0[0] * tmp;
 	    }
-	}
-
-	for (integer i = taylor_sizes[2]; i != taylor_sizes[3]; ++i) {
-	    const auto& tmp = D[i];
-	    A1[i] = -m1[0] * tmp;
-	    A0[i] = m0[0] * tmp;
-	}
 #ifdef USE_GRAV_PAR
-	std::lock_guard<hpx::lcos::local::spinlock> lock(*L_mtx);
+	    std::lock_guard<hpx::lcos::local::spinlock> lock(*L_mtx);
 #endif
 
-	// potential and correction have been computed, no scatter the results
-	for (integer i = 0; i != simd_len && i + li < list_size; ++i) {
-	    const integer iii0 = interaction_list[li + i].first;
-	    const integer iii1 = interaction_list[li + i].second;
+	    // potential and correction have been computed, now scatter the results
+	    for (integer i = 0; i != simd_len && i + interaction_first_index < list_size; ++i) {
+		const integer iii0 = interaction_list[interaction_first_index + i].first;
 
-	    expansion tmp1, tmp2;
+		expansion tmp1;
 #pragma GCC ivdep
-	    for (integer j = 0; j != taylor_sizes[3]; ++j) {
-		tmp1[j] = A0[j][i];
-		tmp2[j] = A1[j][i];
-	    }
-	    L[iii0] += tmp1;
-	    L[iii1] += tmp2;
+		for (integer j = 0; j != taylor_sizes[3]; ++j) {
+		    tmp1[j] = A0[j][i];
+		}
+		L[iii0] += tmp1;
 
-	    if (type == RHO && opts.ang_con) {
-		space_vector& L_ciii0 = L_c[iii0];
-		space_vector& L_ciii1 = L_c[iii1];
-		for (integer j = 0; j != NDIM; ++j) {
-		    L_ciii0[j] += B0[j][i];
-		    L_ciii1[j] += B1[j][i];
+		if (type == RHO && opts.ang_con) {
+		    space_vector& L_ciii0 = L_c[iii0];
+		    for (integer j = 0; j != NDIM; ++j) {
+			L_ciii0[j] += B0[j][i];
+		    }
+		}
+	    }	
+	    for (integer i = 0; i != simd_len && i + interaction_first_index < list_size; ++i) {
+		const integer iii1 = interaction_list[interaction_first_index + i].second;
+
+		expansion tmp2;
+#pragma GCC ivdep
+		for (integer j = 0; j != taylor_sizes[3]; ++j) {
+		    tmp2[j] = A1[j][i];
+		}
+		L[iii1] += tmp2;
+
+		if (type == RHO && opts.ang_con) {
+		    space_vector& L_ciii1 = L_c[iii1];
+		    for (integer j = 0; j != NDIM; ++j) {
+			L_ciii1[j] += B1[j][i];
+		    }
 		}
 	    }
 	}
     }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration = end - start;
+    std::cout << "compute_interactions_inner duration (ms): " << duration.count() << std::endl;
 }
 
 void grid::compute_interactions_leaf(gsolve_type type) {
