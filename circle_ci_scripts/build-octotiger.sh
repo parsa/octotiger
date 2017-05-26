d41635885cc9f588b6fbd311f40870064197e4c2
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-05-26 10:27:04 +0200

    possible CircleCI fix

--- circle_ci_scripts/build-octotiger.sh
+++ circle_ci_scripts/build-octotiger.sh
@@ -4,5 +4,5 @@ set -x
 mkdir build
 cd build
 cmake -DBOOST_ROOT="$Boost_ROOT" -DOCTOTIGER_WITH_SILO=OFF -DCMAKE_PREFIX_PATH="$HPX_ROOT" -DCMAKE_BUILD_TYPE=release ../
-make -j2 VERBOSE=1
+make -j1 VERBOSE=1
 cd ..
