f5a0f363bac0ba40ff233a005477183b18a24f73
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-06-30 16:01:18 +0200

    small fix

--- src/kernels/m2m_simd_types.hpp
+++ src/kernels/m2m_simd_types.hpp
@@ -4,7 +4,7 @@
 
 
 #if defined(Vc_HAVE_AVX512F)
-using m2m_vector = Vc::datapar<double, Vc::datapar_abi::avx512>;
+using m2m_vector = typename Vc::datapar<double, Vc::datapar_abi::avx512>;
 // for 8-wide (and not 16-wide) integers
 using m2m_int_vector = typename Vc::datapar<int32_t, Vc::datapar_abi::avx>;
 // using m2m_int_vector = Vc::datapar<int64_t, Vc::datapar_abi::avx512>;
