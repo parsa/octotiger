da2b5d1bd562c868e648b797334f6d7a41d4ff30
Dominic Marcello <dmarce504@gmail.com>
2017-10-12 08:27:08 -0500

    updated for latest HPX

--- src/node_server_actions_2.cpp
+++ src/node_server_actions_2.cpp
@@ -277,7 +277,7 @@ diagnostics_t node_server::child_diagnostics(const std::pair<space_vector, space
 	for (integer ci = 0; ci != NCHILD; ++ci) {
 		futs[index++] = children[ci].diagnostics(axis, l1, c1, c2);
 	}
-	auto child_sums = hpx::util::unwrapped(futs);
+	auto child_sums = hpx::util::unwrap(futs);
 	return std::accumulate(child_sums.begin(), child_sums.end(), sums);
 }
 
